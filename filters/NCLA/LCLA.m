fdd8315ebed1e0aec81789c962073f6ee18fb443
colson <dud3722000@naver.com>
2020-08-03 19:40:20 +0900

    .

---
+++ filters/NCLA/LCLA.m
@@ -0,0 +1,29 @@
+% paper: A novel cooperative localization algorithm using enhanced particle filter technique in maritime search and rescue wireless sensor network.
+% author: Yeong Jun Kim(colson)
+% email: colson@korea.ac.kr || dud3722000@naver.com
+% date: 2020-07-30
+% Utilezed code: https://www.github.com/YeongJunKim/localization_sim
+
+classdef LCLA < handle
+    properties
+        
+        
+    end
+    methods
+       
+        function obj = LCLA(data)
+            
+        end
+        
+        function r = importance_sampling(obj)
+            
+        end
+        
+        function r = residual_systematic_resampling(obj)
+            
+        end
+    end
+    
+    
+    
+end
